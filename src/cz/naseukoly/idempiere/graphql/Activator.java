66d407c50ce1df76976982c57f33d07243e3c037
David Podhola <david@podhola.net>
2019-05-28 21:18:27 +0200

    WIP

---
+++ src/cz/naseukoly/idempiere/graphql/Activator.java
@@ -0,0 +1,7 @@
+package cz.naseukoly.idempiere.graphql;
+import org.adempiere.plugin.utils.Incremental2PackActivator;
+import org.osgi.framework.BundleActivator;
+
+public class Activator extends Incremental2PackActivator implements BundleActivator {
+
+}
---
+++ src/cz/naseukoly/idempiere/graphql/HelloServlet.java
@@ -0,0 +1,7 @@
+package cz.naseukoly.idempiere.graphql;
+
+import graphql.servlet.OsgiGraphQLHttpServlet;
+
+public class HelloServlet extends OsgiGraphQLHttpServlet {
+
+}
