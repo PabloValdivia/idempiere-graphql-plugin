ca13855feddd58bddf968fa2fb4a7eef339d6661
David Podhola <david@podhola.net>
2019-06-11 06:10:14 +0200

    more

--- src/cz/naseukoly/idempiere/graphql/Activator.java
+++ src/cz/naseukoly/idempiere/graphql/Activator.java
@@ -1,7 +1,20 @@
 package cz.naseukoly.idempiere.graphql;
 import org.adempiere.plugin.utils.Incremental2PackActivator;
 import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
 
 public class Activator extends Incremental2PackActivator implements BundleActivator {
-
+	  private static BundleContext bundleContext;
+	  
+	  @Override
+	  public void start(final BundleContext context) throws Exception {
+	    Activator.bundleContext = context;
+	    super.start(context);
+	  }
+	  
+	  @Override
+	  public void stop(final BundleContext context) throws Exception {
+	    Activator.bundleContext = null;
+	    super.stop(context);
+	  }
 }
